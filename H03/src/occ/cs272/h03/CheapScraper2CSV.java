eb3edcb87d56074c67634e2bf0d3c2b8e959969f
Usman <Usman@10.0.93.18>
2019-09-25 12:12:51 -0700

    Updated

--- H03/src/occ/cs272/h03/CheapScraper2CSV.java
+++ H03/src/occ/cs272/h03/CheapScraper2CSV.java
@@ -10,11 +10,14 @@ package occ.cs272.h03;
 public class CheapScraper2CSV {
 
 	public static void main(String[] args) {
-		System.out.println("Command Line Args");
+		System.out.println("Command Line Args: ");
 		for (int i = 0; i < args.length; ++i)
 		{
 			System.out.println(args[i]);
 		}
+		int columns = Integer.parseInt(args[0]);
+		String inputFileName = args[1];
+		String outputFileName = args[2];
 	}
 
 }
