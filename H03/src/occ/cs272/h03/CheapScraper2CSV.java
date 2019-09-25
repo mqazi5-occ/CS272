a6671c6ea349daeb114bb26e2e27f95c926bf355
Usman <Usman@10.0.93.18>
2019-09-25 12:01:29 -0700

    Initial Commit

---
+++ H03/src/occ/cs272/h03/CheapScraper2CSV.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package occ.cs272.h03;
+
+/**
+ * @author Mohammad Usman Qazi
+ * @version Sep 25, 2019
+ */
+public class CheapScraper2CSV {
+
+	public static void main(String[] args) {
+		System.out.println("Command Line Args");
+		for (int i = 0; i < args.length; ++i)
+		{
+			System.out.println(args[i]);
+		}
+	}
+
+}
