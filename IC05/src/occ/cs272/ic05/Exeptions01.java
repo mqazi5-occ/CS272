3e19f32c650f19a54587401d0687cf237cc92afc
Usman <Usman@10.0.93.18>
2019-09-25 13:34:21 -0700

    Completed

--- IC05/src/occ/cs272/ic05/Exeptions01.java
+++ IC05/src/occ/cs272/ic05/Exeptions01.java
@@ -21,12 +21,39 @@ public class Exeptions01
             
             int [] ar = {5, 6, 7};
             
-            String indexStr = cin.next();
-            int index = Integer.parseInt(indexStr);
-    //        int index = System.in.read();
-    
-            int value = ar[index];
-            System.out.printf("ar[%d] = %d%n", index, value);
+            try {
+//				String indexStr = cin.next();
+//				int index = Integer.parseInt(indexStr);
+				int index = System.in.read();
+   
+				int value = ar[index];
+				System.out.printf("ar[%d] = %d%n", index, value);
+			} 
+//          catch (NumberFormatException e) 
+//          {
+//				//e.printStackTrace();
+//				System.err.println("Please use an integer input.");
+//			}
+//            catch (ArrayIndexOutOfBoundsException e) 
+//            {
+//				System.err.println("Please use an integer between 0 and " + (ar.length-1));
+//			}
+            catch (Exception e)
+            {
+            	if (e instanceof NumberFormatException)
+            	{
+    				System.err.println("Please use an integer input");
+    				
+            	}
+            	else if (e instanceof ArrayIndexOutOfBoundsException)
+            	{
+            		System.err.println("Please use an integer between 0 and " + (ar.length-1));
+            	}
+            	else
+            	{
+            		System.err.println("Aha I gotcha");
+            	}
+            }
         }
     }
 }
